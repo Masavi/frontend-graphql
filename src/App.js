242defedc9c88bac6bf776b3c504affcfa0b301a
maui <maui@devf.mx>
2019-05-14 21:09:07 -0500

    add HolaMundo component example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HolaMundo from './components/HolaMundo';
 
 function App() {
   return (
@@ -9,6 +10,10 @@ function App() {
             <p>Esta es otra etiqueta</p>
             <a href="google.com">Clickeame (no hago nada jejej)</a>
           </div>
+          <HolaMundo />
+          <HolaMundo />
+          <HolaMundo />
+          <HolaMundo />
       </React.Fragment>
   );
 }
---
+++ src/components/HolaMundo.js
@@ -0,0 +1,13 @@
+//imrc
+import React, { Component } from 'react';
+
+//cc
+class HolaMundo extends Component {
+    render() { 
+        return ( 
+            <h1>Hola desde el componente 'HolaMundo'</h1>
+        );
+    }
+}
+ 
+export default HolaMundo;
